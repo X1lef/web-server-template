7f93026b0c8b68948f962112e6a3882d397f5776
Felix Pedrozo <pedrozofelix6991@gmail.com>
2022-07-02 23:03:41 -0400

    refactor: :recycle: Se elimina lineas de codigo innecesaria

--- src/app.js
+++ src/app.js
@@ -3,9 +3,6 @@ import express from 'express';
 
 const port = process.env.PORT || 8084;
 const app = express();
-
 app.use(express.static('public'));
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+app.listen(port, () => console.log(`Server listening on port ${port}`));
